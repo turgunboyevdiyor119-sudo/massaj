b42d13b12b3e66ca6ef474fe0f11ea97ad7a5bc4
Diyor Turgunboyev <turgunboyevdiyor119@gmail.com>
2026-08-27 13:27:56 +0500

    Add vercel.json deployment config for global internet hosting

--- server.js
+++ server.js
@@ -459,8 +459,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${PORT} (Accessible on network & phones)`);
   // Run db check to seed
   getDB();
 });
